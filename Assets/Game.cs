5560fcfb2979155a70ffd6dbaa24c97288b29582
Sabaniki <sawada13@mccc.jp>
2020-05-18 23:29:05 +0900

    課題5まで完成。 ボールがブロックに当たると偽物のボール(見分けはつかないし壁に反射はするがブロックとプレイヤーへの当たり判定がない)が生成されるようにした。

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using GameCanvas;
 using UnityEngine;
 using Sequence = System.Collections.IEnumerator;
@@ -13,8 +11,11 @@ using Sequence = System.Collections.IEnumerator;
 public sealed class Game : GameBase {
     // 変数の宣言
     int sec = 0;
+
     public Vector2 screenSize;
-    private Ball ball;
+
+    // private Ball ball;
+    public List<Ball> balls;
     private Player player;
     private List<Block> blocks;
     private bool gameCleared;
@@ -25,7 +26,9 @@ public sealed class Game : GameBase {
     /// </summary>
     public override void InitGame() {
         screenSize = new Vector2(640, 480);
-        ball = new Ball(new Vector2(0, 0), new Vector2(3, 3), screenSize);
+        balls = new List<Ball> {
+            new Ball(gc, new Vector2(100, 100), new Vector2(3, 3), screenSize),
+        };
         player = new Player(new Vector2(270, 460), new Vector2(100, 20));
         // キャンバスの大きさを設定します
         gc.SetResolution((int) screenSize.x, (int) screenSize.y);
@@ -34,26 +37,33 @@ public sealed class Game : GameBase {
             blocks.Add(new Block(new Vector2(((i % 10) * Block.Size.x), ((i / 10) * Block.Size.y))));
         }
 
-        ball.UpdateCallback += () => {
-            if (gc.CheckHitRect(
-                (int) ball.Position.x, (int) ball.Position.y, ball.BallRadius, ball.BallRadius,
-                (int) player.Position.x, (int) player.Position.y, (int) player.Size.x, (int) player.Size.y
-            )) {
-                ball.Speed.y = -ball.Speed.y;
-            }
-        };
-
-        ball.UpdateCallback += () => {
-            blocks.ForEach(block => {
+        balls.ForEach(ball => {
+            ball.UpdateCallback += () => {
                 if (gc.CheckHitRect(
                     (int) ball.Position.x, (int) ball.Position.y, ball.BallRadius, ball.BallRadius,
-                    (int) block.Position.x, (int) block.Position.y, (int) Block.Size.x, (int) Block.Size.y
-                ) && block.IsAlive) {
+                    (int) player.Position.x, (int) player.Position.y, (int) player.Size.x, (int) player.Size.y
+                )) {
                     ball.Speed.y = -ball.Speed.y;
-                    block.IsAlive = false;
                 }
-            });
-        };
+            };
+
+            ball.UpdateCallback += () => {
+                blocks.ForEach(block => {
+                    if (gc.CheckHitRect(
+                        (int) ball.Position.x, (int) ball.Position.y, ball.BallRadius, ball.BallRadius,
+                        (int) block.Position.x, (int) block.Position.y, (int) Block.Size.x, (int) Block.Size.y
+                    ) && block.IsAlive) {
+                        ball.Speed.y = -ball.Speed.y;
+                        var ballCopy = new Ball(ball) {
+                            Position = new Vector2(block.Position.x, block.Position.y - 100), Speed = new Vector2(Random.value * 5, Random.value * 5)
+                        };
+                        balls.Add(ballCopy);
+                        block.IsAlive = false;
+                    }
+                });
+            };
+        });
+
         gameCleared = false;
     }
 
@@ -62,11 +72,14 @@ public sealed class Game : GameBase {
     /// 動きなどの更新処理
     /// </summary>
     public override void UpdateGame() {
-        ball.Update();
+        balls.ToList().ForEach(ball => ball.Update()); 
+        balls.ToList().ForEach(ball => {
+            if (ball.IsAlive) gc.DrawImage(1, (int) ball.Position.x, (int) ball.Position.y);
+        });
+
         player.Update(gc.GetPointerFrameCount(0), gc.GetPointerX(0), gc.GetPointerY(0));
-        gameCleared = blocks.All(block => !block.IsAlive);
         // 起動からの経過時間を取得します
-        if(!gameCleared) sec = (int) gc.TimeSinceStartup;
+        if (!gameCleared) sec = (int) gc.TimeSinceStartup;
     }
 
     /// <summary>
@@ -77,8 +90,13 @@ public sealed class Game : GameBase {
         gc.ClearScreen();
         gc.DrawImage(0, 0, 0);
         // 0番の画像を描画します
-        if (ball.IsAlive) gc.DrawImage(1, (int) ball.Position.x, (int) ball.Position.y);
-        else gc.DrawString("Game Over!!", (int)screenSize.x / 2, (int)screenSize.y / 2);
+        balls.ToList().ForEach(ball => {
+            if (ball.IsAlive) gc.DrawImage(1, (int) ball.Position.x, (int) ball.Position.y);
+        });
+        if (!balls.ToList().Any(ball => ball.IsAlive) && !gameCleared) {
+            gc.DrawString("Game Over!!", (int) screenSize.x / 2, (int) screenSize.y / 2);
+        }
+
 
         gc.SetColor(0, 0, 255);
         gc.FillRect((int) player.Position.x, (int) player.Position.y, (int) player.Size.x, (int) player.Size.y);
@@ -90,16 +108,12 @@ public sealed class Game : GameBase {
         }
 
         if (gameCleared) {
-            gc.DrawString($"Game Clear!! Point: {sec}", (int)screenSize.x / 2, (int)screenSize.y / 2);
+            gc.DrawString($"Game Clear!! time: {sec}s", (int) screenSize.x / 2, (int) screenSize.y / 2);
         }
-
     }
 }
 
-internal interface IGameObject {
-}
-
-internal class Ball : IGameObject {
+public class Ball {
     public Vector2 Position;
 
     public Vector2 Speed;
@@ -113,20 +127,30 @@ internal class Ball : IGameObject {
     public UpdateBallCallback UpdateCallback;
 
     private readonly Proxy proxy;
-    
+
     public bool IsAlive { get; private set; }
 
 
-    public Ball(Vector2 position, Vector2 speed, Vector2 resolution) {
+    public Ball(Proxy proxy, Vector2 position, Vector2 speed, Vector2 resolution) {
+        this.proxy = proxy;
         this.Position = position;
         this.Speed = speed;
         this.resolution = resolution;
         BallRadius = 24;
         IsAlive = true;
     }
+    
+    public Ball(Ball ball) {
+        this.proxy = ball.proxy;
+        this.Position = ball.Position;
+        this.Speed = ball.Speed;
+        this.resolution = ball.resolution;
+        BallRadius = 24;
+        IsAlive = true;
+    }
 
     public void Update() {
-        UpdateCallback();
+        UpdateCallback?.Invoke();
         Position.x += Speed.x;
         Position.y += Speed.y;
         if (Position.x < 0) {
@@ -149,7 +173,7 @@ internal class Ball : IGameObject {
     }
 }
 
-internal class Player : IGameObject {
+internal class Player {
     public Vector2 Position;
     public Vector2 Size;
 
@@ -159,10 +183,9 @@ internal class Player : IGameObject {
     }
 
     public void Update(int pointerFrameCount, int touchedPointerX, int touchedPointerY) {
-        if (pointerFrameCount > 0) {
-            Position.x = touchedPointerX - Size.x / 2;
-            Position.y = touchedPointerY - Size.y / 2;
-        }
+        if (pointerFrameCount <= 0) return;
+        Position.x = touchedPointerX - Size.x / 2;
+        Position.y = touchedPointerY - Size.y / 2;
     }
 }
 
